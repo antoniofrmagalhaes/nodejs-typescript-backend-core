eb779f144377e9b1bc0673d123458812f9e98299
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-08-28 11:58:34 -0400

    Changed to class App

--- src/core/app.ts
+++ src/core/app.ts
@@ -2,8 +2,25 @@ import express from 'express';
 
 import routes from './routes';
 
-const app = express();
+class App {
+  public server: express.Application;
 
-app.use(routes);
+  constructor() {
+    this.server = express();
 
-export default app;
+    this.middlewares();
+    this.routes();
+    this.database();
+  }
+
+  private middlewares(): void {
+    this.server.use(express.json());
+  }
+  private database(): void {}
+
+  private routes(): void {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
